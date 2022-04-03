38d4a95f1e8fc7a96ac00f4e20b918923128ed8d
Vinci23 <88689746+Vinci23@users.noreply.github.com>
2022-04-03 19:03:50 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace systemIo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite putanju direktorija: ");
+            string putanja = Console.ReadLine();
+
+            //provjera da li direktorij postoji
+            if (Directory.Exists(putanja))
+            {
+                //ako postoji zapisujemo sve u njega
+                Console.WriteLine("Datoteke: ");
+                foreach (string datoteka in Directory.GetFiles(putanja))
+                {
+                    Console.WriteLine(datoteka);
+                }
+
+            }
+            string putanjaPodDirektorija = putanja + "\\Test";
+
+            if (!Directory.Exists(putanjaPodDirektorija))
+            {
+                Directory.CreateDirectory(putanjaPodDirektorija);
+            }
+            else
+            {
+                Console.WriteLine("Direktorij već postoji");
+            }
+
+
+            if (Directory.Exists(putanja))
+            {
+                Console.WriteLine("Poddirektoriji: ");
+                foreach (string datoteka in Directory.GetDirectories(putanja))
+                {
+                    Console.WriteLine(datoteka);
+                }
+            }
+
+            Directory.Delete(putanjaPodDirektorija);
+
+            if (Directory.Exists(putanja))
+            {
+                Console.WriteLine("Nakon brisanja: ");
+                foreach (string datoteka in Directory.GetDirectories(putanja))
+                {
+                    Console.WriteLine(datoteka);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
